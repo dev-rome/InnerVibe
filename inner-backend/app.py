1fe7cdd49b7b302653ad35454682684141bbd52e
dev-rome <haynesjerome@gmail.com>
2024-01-19 13:11:27 -0500

    Add Flask SQLAlchemy and dotenv for PostgreSQL database setup

--- inner-backend/app.py
+++ inner-backend/app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 
 @app.route("/")
 def home():
