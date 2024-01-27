b08f9f9b06cbecc3b9a3a88b3e14cfe7faa4ecd8
dev-rome <haynesjerome@gmail.com>
2024-01-26 19:37:43 -0500

    Integrate Flask-Migrate and models

--- inner-backend/app.py
+++ inner-backend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from models import User, MoodEntry
 from dotenv import load_dotenv
 import os
 
@@ -9,6 +11,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route("/")
